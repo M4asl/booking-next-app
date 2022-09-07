974de93f343d1d327dfb42df4b48438e50670033
Mateusz Masłowiec <mateuszmaslowiec@MacBook-Pro-Mateusz.local>
2022-09-07 15:14:32 +0200

    Add all rooms and room details actions,reducers,constants

---
+++ redux/actions/roomActions.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import absoluteUrl from 'next-absolute-url';
+
+import {
+  ALL_ROOMS_SUCCESS,
+  ALL_ROOMS_FAIL,
+  CLEAR_ERRORS,
+} from '../constants/roomConstants';
+
+// Get all rooms
+export const getRooms =
+  (req, currentPage = 1, location = '', guests, category) =>
+  async (dispatch) => {
+    try {
+      const { origin } = absoluteUrl(req);
+
+      let link = `${origin}/api/rooms?page=${currentPage}&location=${location}`;
+
+      if (guests) link = link.concat(`&guestCapacity=${guests}`);
+      if (category) link = link.concat(`&category=${category}`);
+
+      const { data } = await axios.get(link);
+
+      dispatch({
+        type: ALL_ROOMS_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ALL_ROOMS_FAIL,
+        payload: error.response.data.message,
+      });
+    }
+  };
+
+export const getRoomDetails = (req, id) => async (dispatch) => {
+  try {
+    const { origin } = absoluteUrl(req);
+
+    const url = `${origin}/api/rooms/${id}`;
+
+    const { data } = await axios.get(url);
+
+    dispatch({
+      type: ROOM_DETAILS_SUCCESS,
+      payload: data.room,
+    });
+  } catch (error) {
+    dispatch({
+      type: ROOM_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
