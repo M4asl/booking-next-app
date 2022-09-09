3f65ab1e74ac731c3b5de7135928d3bfb908b515
Mateusz Masłowiec <mateuszmaslowiec@MBP-Mateusz.home>
2022-09-09 23:31:25 +0200

    Update room facilities view

--- components/room/RoomFacilities.js
+++ components/room/RoomFacilities.js
@@ -1,82 +1,90 @@
 import React from 'react';
-import { GrGroup } from 'react-icons/gr';
+import { IconContext } from 'react-icons';
+import { HiOutlineUserGroup, HiOutlineWifi } from 'react-icons/hi';
+import { IoBedOutline, IoSnowOutline } from 'react-icons/io5';
+import {
+  MdOutlineBreakfastDining,
+  MdOutlinePets,
+  MdOutlineCleaningServices,
+} from 'react-icons/md';
 
 const RoomFacilities = ({ room }) => {
+  const iconStyle = { color: '#EEA86C', size: '20px' };
+  const roomFacilitiesContainerStyle = {
+    display: 'flex',
+    maxWidth: '500px',
+    flexWrap: 'wrap',
+  };
+  const roomFacilitiesStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    margin: '5px 10px 5px 0px',
+    fontWeight: '300',
+    letterSpacing: '2px',
+    backgroundColor: '#ffffff',
+    width: 'fit-content',
+    padding: '10px',
+    borderRadius: '15px',
+    color: '#282828',
+  };
   return (
-    <div className="features mt-5">
-      <h3 className="mb-4">Features:</h3>
-      <div className="room-feature">
-        <i
-          className="fa fa-cog fa-fw fa-users"
-          aria-hidden="true"
-        ></i>
-        <p>{room.guestCapacity} Guests</p>
+    <div style={roomFacilitiesContainerStyle}>
+      <div style={roomFacilitiesStyle}>
+        <IconContext.Provider value={iconStyle}>
+          <HiOutlineUserGroup />
+        </IconContext.Provider>
+        <span className="ml-3">{room.guestCapacity} Guests</span>
       </div>
 
-      <div className="room-feature">
-        <i className="fa fa-cog fa-fw fa-bed" aria-hidden="true"></i>
-        <p>{room.numOfBeds} Beds</p>
+      <div style={roomFacilitiesStyle}>
+        <IconContext.Provider value={iconStyle}>
+          <IoBedOutline />
+        </IconContext.Provider>
+        <span className="ml-3">{room.numOfBeds} Beds</span>
       </div>
 
-      <div className="room-feature">
-        <i
-          className={
-            room.breakfast
-              ? 'fa fa-check text-success'
-              : 'fa fa-times text-danger'
-          }
-          aria-hidden="true"
-        ></i>
-        <p>Breakfast</p>
-      </div>
+      {room.breakfast && (
+        <div style={roomFacilitiesStyle}>
+          <IconContext.Provider value={iconStyle}>
+            <MdOutlineBreakfastDining />
+          </IconContext.Provider>
+          <span className="ml-3">Breakfast</span>
+        </div>
+      )}
 
-      <div className="room-feature">
-        <i
-          className={
-            room.internet
-              ? 'fa fa-check text-success'
-              : 'fa fa-times text-danger'
-          }
-          aria-hidden="true"
-        ></i>
-        <p>Internet</p>
-      </div>
+      {room.internet && (
+        <div style={roomFacilitiesStyle}>
+          <IconContext.Provider value={iconStyle}>
+            <HiOutlineWifi />
+          </IconContext.Provider>
+          <span className="ml-3">Internet</span>
+        </div>
+      )}
 
-      <div className="room-feature">
-        <i
-          className={
-            room.airConditioned
-              ? 'fa fa-check text-success'
-              : 'fa fa-times text-danger'
-          }
-          aria-hidden="true"
-        ></i>
-        <p>Air Conditioned</p>
-      </div>
-
-      <div className="room-feature">
-        <i
-          className={
-            room.petsAllowed
-              ? 'fa fa-check text-success'
-              : 'fa fa-times text-danger'
-          }
-          aria-hidden="true"
-        ></i>
-        <p>Pets Allowed</p>
-      </div>
-
-      <div className="room-feature">
-        <i
-          className={
-            room.roomCleaning
-              ? 'fa fa-check text-success'
-              : 'fa fa-times text-danger'
-          }
-          aria-hidden="true"
-        ></i>
-        <p>Room Cleaning</p>
-      </div>
+      {room.airConditioned && (
+        <div style={roomFacilitiesStyle}>
+          <IconContext.Provider value={iconStyle}>
+            <IoSnowOutline />
+          </IconContext.Provider>
+          <span className="ml-3">Air Conditioned</span>
+        </div>
+      )}
+      {room.petsAllowed && (
+        <div style={roomFacilitiesStyle}>
+          <IconContext.Provider value={iconStyle}>
+            <MdOutlinePets />
+          </IconContext.Provider>
+          <span className="ml-3">Pets Allowed</span>
+        </div>
+      )}
+      {room.roomCleaning && (
+        <div style={roomFacilitiesStyle}>
+          <IconContext.Provider value={iconStyle}>
+            <MdOutlineCleaningServices />
+          </IconContext.Provider>
+          <span className="ml-3">Room Cleaning</span>
+        </div>
+      )}
     </div>
   );
 };
