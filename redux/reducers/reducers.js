7e8dcf6777ae7154ceb0d6ac3141241adde0be67
Mateusz Masłowiec <mateusz.maslowiec98@gmail.com>
2022-09-15 21:17:36 +0200

    Create update profile, forgot password, reset password redux state and actions

--- redux/reducers/reducers.js
+++ redux/reducers/reducers.js
@@ -1,12 +1,20 @@
 import { combineReducers } from 'redux';
 
 import { allRoomsReducer, roomDetailsReducer } from './roomReducers';
-import { authReducer } from './userReducers';
+import {
+  authReducer,
+  forgotPasswordReducer,
+  loadedUserReducer,
+  userReducer,
+} from './userReducers';
 
 const reducers = combineReducers({
   allRooms: allRoomsReducer,
   roomDetails: roomDetailsReducer,
   auth: authReducer,
+  user: userReducer,
+  loadedUser: loadedUserReducer,
+  forgotPassword: forgotPasswordReducer,
 });
 
 export default reducers;
