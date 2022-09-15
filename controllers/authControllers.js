71393be29460bef448c3b0b996a4b3c4928d9ee7
Mateusz Masłowiec <mateusz.maslowiec98@gmail.com>
2022-09-15 21:14:57 +0200

    Create update profile, forgot password, reset password controllers

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,6 +2,11 @@ import User from '../models/userModel';
 import cloudinary from 'cloudinary';
 
 import catchAsyncErrors from '../middlewares/catchAsyncErrors';
+import sendEmail from '../utils/sendEmail';
+import ErrorHandler from '../utils/errorHandler';
+
+import absoluteUrl from 'next-absolute-url';
+import crypto from 'crypto';
 
 // Setting up cloudinary config
 cloudinary.config({
@@ -49,4 +54,134 @@ const currentUserProfile = catchAsyncErrors(async (req, res) => {
   });
 });
 
-export { registerUser, currentUserProfile };
+// Update user profile   =>   /api/me/update
+const updateProfile = catchAsyncErrors(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name;
+    user.email = req.body.email;
+
+    if (req.body.password) user.password = req.body.password;
+  }
+
+  // Update avatar
+  if (req.body.avatar !== '') {
+    const image_id = user.avatar.public_id;
+
+    // Delete user previous image/avatar
+    await cloudinary.v2.uploader.destroy(image_id);
+
+    const result = await cloudinary.v2.uploader.upload(
+      req.body.avatar,
+      {
+        folder: 'bookingApp/avatars',
+        width: '150',
+        crop: 'scale',
+      }
+    );
+
+    user.avatar = {
+      public_id: result.public_id,
+      url: result.secure_url,
+    };
+  }
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Forgot password   =>   /api/password/forgot
+const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(
+      new ErrorHandler('User not found with this email', 404)
+    );
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Get origin
+  const { origin } = absoluteUrl(req);
+
+  // Create reset password url
+  const resetUrl = `${origin}/password/reset/${resetToken}`;
+
+  const message = `Your password reset url is as follow: \n\n ${resetUrl} \n\n\ If you have not requested this email, then ignore it.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'BookingApp Password Recovery',
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to: ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// Reset password   =>   /api/password/reset/:token
+const resetPassword = catchAsyncErrors(async (req, res, next) => {
+  // Hash URL token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.query.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        'Password reset token is invalid or has been expired',
+        400
+      )
+    );
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler('Password does not match', 400));
+  }
+
+  // Setup the new password
+  user.password = req.body.password;
+
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: 'Password updated successfully',
+  });
+});
+
+export {
+  registerUser,
+  currentUserProfile,
+  updateProfile,
+  forgotPassword,
+  resetPassword,
+};
