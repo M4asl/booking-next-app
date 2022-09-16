36f42b2e3cfcab6cf5145872ec5bc2ee4feb5dc0
Mateusz Masłowiec <mateusz.maslowiec98@gmail.com>
2022-09-16 21:06:46 +0200

    Add new booking, chceck if room is availability, check dates of room backend actions

---
+++ controllers/bookingControllers.js
@@ -0,0 +1,118 @@
+import Booking from '../models/bookingModel';
+
+import ErrorHandler from '../utils/errorHandler';
+import catchAsyncErrors from '../middlewares/catchAsyncErrors';
+
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
+
+const moment = extendMoment(Moment);
+
+// Create new Booking   =>   /api/bookings
+const newBooking = catchAsyncErrors(async (req, res) => {
+  const {
+    room,
+    checkInDate,
+    checkOutDate,
+    daysOfStay,
+    amountPaid,
+    paymentInfo,
+  } = req.body;
+
+  const booking = await Booking.create({
+    room,
+    user: req.user._id,
+    checkInDate,
+    checkOutDate,
+    daysOfStay,
+    amountPaid,
+    paymentInfo,
+    paidAt: Date.now(),
+  });
+
+  res.status(200).json({
+    success: true,
+    booking,
+  });
+});
+
+// Create new booking   =>   /api/bookings/check
+const checkRoomBookingAvailability = catchAsyncErrors(
+  async (req, res) => {
+    let { roomId, checkInDate, checkOutDate } = req.query;
+
+    checkInDate = new Date(checkInDate);
+    checkOutDate = new Date(checkOutDate);
+
+    const bookings = await Booking.find({
+      room: roomId,
+      $and: [
+        {
+          checkInDate: {
+            $lte: checkOutDate,
+          },
+        },
+        {
+          checkOutDate: {
+            $gte: checkInDate,
+          },
+        },
+      ],
+    });
+
+    // Check if there is any booking available
+    let isAvailable;
+
+    if (bookings && bookings.length === 0) {
+      isAvailable = true;
+    } else {
+      isAvailable = false;
+    }
+
+    res.status(200).json({
+      success: true,
+      isAvailable,
+    });
+  }
+);
+
+// Check booked dates of a room   =>   /api/bookings/check_booked_dates
+const checkBookedDatesOfRoom = catchAsyncErrors(async (req, res) => {
+  const { roomId } = req.query;
+
+  const bookings = await Booking.find({ room: roomId });
+
+  let bookedDates = [];
+
+  const timeDiffernece = moment().utcOffset() / 60;
+
+  bookings.forEach((booking) => {
+    const checkInDate = moment(booking.checkInDate).add(
+      timeDiffernece,
+      'hours'
+    );
+    const checkOutDate = moment(booking.checkOutDate).add(
+      timeDiffernece,
+      'hours'
+    );
+
+    const range = moment.range(
+      moment(checkInDate),
+      moment(checkOutDate)
+    );
+
+    const dates = Array.from(range.by('day'));
+    bookedDates = bookedDates.concat(dates);
+  });
+
+  res.status(200).json({
+    success: true,
+    bookedDates,
+  });
+});
+
+export {
+  newBooking,
+  checkRoomBookingAvailability,
+  checkBookedDatesOfRoom,
+};
