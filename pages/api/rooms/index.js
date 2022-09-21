2ca4350a320d0e87b74e03b8007383e8b9ec12ba
M4asl <mateusz.maslowiec98@gmail.com>
2022-09-21 16:18:14 +0200

    New room route

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,17 +1,25 @@
 import nc from 'next-connect';
 import dbConnect from '../../../config/dbConnect';
+
 import {
   allRooms,
-  createRoom,
+  newRoom,
 } from '../../../controllers/roomControllers';
 
 import onError from '../../../middlewares/errors';
+import {
+  isAuthenticatedUser,
+  authorizeRoles,
+} from '../../../middlewares/auth';
 
 const handler = nc({ onError });
 
 dbConnect();
 
 handler.get(allRooms);
-handler.post(createRoom);
+
+handler
+  .use(isAuthenticatedUser, authorizeRoles('admin'))
+  .post(newRoom);
 
 export default handler;
