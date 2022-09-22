ca115f48b63fd4354b8cbc6d8af9036d53177cc1
M4asl <mateusz.maslowiec98@gmail.com>
2022-09-22 12:54:12 +0200

    Add middlewares to update & delete routes

--- pages/api/rooms/[id].js
+++ pages/api/rooms/[id].js
@@ -1,19 +1,30 @@
 import nc from 'next-connect';
 import dbConnect from '../../../config/dbConnect';
+
 import {
-  deleteRoom,
   getSingleRoom,
   updateRoom,
+  deleteRoom,
 } from '../../../controllers/roomControllers';
 
 import onError from '../../../middlewares/errors';
+import {
+  isAuthenticatedUser,
+  authorizeRoles,
+} from '../../../middlewares/auth';
 
 const handler = nc({ onError });
 
 dbConnect();
 
 handler.get(getSingleRoom);
-handler.put(updateRoom);
-handler.delete(deleteRoom);
+
+handler
+  .use(isAuthenticatedUser, authorizeRoles('admin'))
+  .put(updateRoom);
+
+handler
+  .use(isAuthenticatedUser, authorizeRoles('admin'))
+  .delete(deleteRoom);
 
 export default handler;
