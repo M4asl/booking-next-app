ce4166cf7cda141a15203c809c5d26c22ddfc461
M4asl <mateusz.maslowiec98@gmail.com>
2022-09-23 09:42:25 +0200

    Modify room reducer and add delete room

--- redux/reducers/roomReducers.js
+++ redux/reducers/roomReducers.js
@@ -22,6 +22,10 @@ import {
   UPDATE_ROOM_SUCCESS,
   UPDATE_ROOM_RESET,
   UPDATE_ROOM_FAIL,
+  DELETE_ROOM_REQUEST,
+  DELETE_ROOM_SUCCESS,
+  DELETE_ROOM_RESET,
+  DELETE_ROOM_FAIL,
 } from '../constants/roomConstants';
 
 // All rooms reducer
@@ -193,6 +197,7 @@ export const newRoomReducer = (state = { room: {} }, action) => {
 export const roomReducer = (state = {}, action) => {
   switch (action.type) {
     case UPDATE_ROOM_REQUEST:
+    case DELETE_ROOM_REQUEST:
       return {
         loading: true,
       };
@@ -203,12 +208,25 @@ export const roomReducer = (state = {}, action) => {
         isUpdated: action.payload,
       };
 
+    case DELETE_ROOM_SUCCESS:
+      return {
+        loading: false,
+        isDeleted: action.payload,
+      };
+
     case UPDATE_ROOM_RESET:
       return {
         isUpdated: false,
       };
 
+    case DELETE_ROOM_RESET:
+      return {
+        loading: false,
+        isDeleted: false,
+      };
+
     case UPDATE_ROOM_FAIL:
+    case DELETE_ROOM_FAIL:
       return {
         loading: false,
         error: action.payload,
